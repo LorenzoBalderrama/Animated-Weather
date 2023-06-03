deffb67e51f639cf2bb4a4c68b18a29aca6bdfe5
Zo <70714420+LorenzoBalderrama@users.noreply.github.com>
2023-06-02 18:36:44 -0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Lorenzo Porras on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
